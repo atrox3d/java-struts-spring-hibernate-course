6e877a3023887c7da26e31a861f226f28c250f54
atrox3d <atrox3d@gmail.com>
2016-02-24 14:19:27 +0100

    Create ejb-remote-server project

---
+++ ejb-remote-server/src/main/java/com/illucit/ejbremote/server/ExampleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.illucit.ejbremote.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class ExampleServiceImpl implements ExampleService {
+
+	@Override
+	public String greet(String name) {
+		return "Hello " + name + "!";
+	}
+
+	@Override
+	public Map<Object, Object> getSystemProperties() {
+		return new HashMap<>(System.getProperties());
+	}
+
+}
