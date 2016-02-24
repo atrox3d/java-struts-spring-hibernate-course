a473bf57fd9bb1181197345fbaccc7fb985ee39e
atrox3d <atrox3d@gmail.com>
2016-02-24 19:03:52 +0100

    IT WORKS

--- ejb-remote-client/src/main/java/com/illucit/ejbremote/EjbRemoteClient.java
+++ ejb-remote-client/src/main/java/com/illucit/ejbremote/EjbRemoteClient.java
@@ -1,13 +1,21 @@
 package com.illucit.ejbremote;
 
 import java.util.Hashtable;
+import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import org.jboss.ejb.client.ContextSelector;
+import org.jboss.ejb.client.EJBClientConfiguration;
+import org.jboss.ejb.client.EJBClientContext;
+import org.jboss.ejb.client.PropertiesBasedEJBClientConfiguration;
+import org.jboss.ejb.client.remoting.ConfigBasedEJBClientContextSelector;
+
 import com.illucit.ejbremote.server.ExampleService;
 import com.illucit.ejbremote.server.ExampleServiceImpl;
+import com.illucit.ejbremote.utility.EjbRemoteUtil;
 
 public class EjbRemoteClient {
 
@@ -18,77 +26,45 @@ public class EjbRemoteClient {
 		Context remotingContext;
 		ExampleService service;
 		
-		ejbURL = getEjbURL(
-								"", 
+		ejbURL = EjbRemoteUtil.getEjbURL(
+								"ejb-remote-server", 
 								"ejb-remote-server", 
 								ExampleServiceImpl.class.getSimpleName(), 
 								ExampleService.class.getName()
 							);
 		
+		ejbURL = 
+				"ejb:/ejb-remote-server/ExampleServiceImpl!com.illucit.ejbremote.server.ExampleService";
+		
 		System.out.printf("host  : %s\n", host);
 		System.out.printf("port  : %s\n", port);
 		System.out.printf("ejbURL: %s\n", ejbURL);
 		
-		remotingContext = createRemoteEjbContext(host, port);
+		Properties clientProperties = EjbRemoteUtil.createclientProperties(host, port);
 		
-		service = createEjbProxy(remotingContext, ejbURL, ExampleService.class);
+		remotingContext = EjbRemoteUtil.createRemoteEjbContext(clientProperties);
+		service = EjbRemoteUtil.createEjbProxy(remotingContext, ejbURL, ExampleService.class);
 		
+//	    EJBClientConfiguration ejbClientConfiguration = 
+//	    		new PropertiesBasedEJBClientConfiguration(clientProperties);
+//	    
+//	    ContextSelector<EJBClientContext> contextSelector = 
+//	    		new ConfigBasedEJBClientContextSelector(ejbClientConfiguration);
+//	    
+//	    EJBClientContext.setSelector(contextSelector);
+//	    
+//	    Properties properties = new Properties();
+//	    properties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+//		clientProperties.put("jboss.naming.client.ejb.context", "true");	    
+	    Context context = new InitialContext(clientProperties);
+	    
+	    service = (ExampleService) context.lookup(ejbURL);
+	    
 		String toGreet = "RRrrrobbb!";
 		String exampleResult;
 		
 		exampleResult = service.greet(toGreet);
-	}
-
-	@SuppressWarnings("unchecked")
-	private static <T> T createEjbProxy(
-											Context remotingContext, 
-											String ejbUrl, 
-											Class<T> ejbInterfaceClass
-										)
-			throws NamingException, ClassCastException {
-		Object resolvedproxy = remotingContext.lookup(ejbUrl);
-		return (T) resolvedproxy;
-	}
-	
-	private static String getEjbURL(
-										String appName,
-										String moduleName,
-										String beanName,
-										String remoteView
-									) 
-	{
-		String protocol = "ejb:";
-		String ejbURL;
 		
-		ejbURL = String.format("%s/%s/%s/%s!%s", 
-									protocol,
-									appName,
-									moduleName,
-									beanName,
-									remoteView
-							);
-		return ejbURL;
-	}
-	
-	private static Context createRemoteEjbContext(String host, String port) throws NamingException {
-	
-		Hashtable<Object, Object> props = new Hashtable<>();
-	
-		props.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-		props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-	
-		props.put("jboss.naming.client.ejb.context", false);
-		props.put("org.jboss.ejb.client.scoped.context", true);
-	
-		props.put("endpoint.name", "client-endpoint");
-		props.put("remote.connectionprovider.create.options.org.xnio.Options.SSL_ENABLED", false);
-		props.put("remote.connections", "default");
-		props.put("remote.connection.default.connect.options.org.xnio.Options.SASL_POLICY_NOANONYMOUS", false);
-	
-		props.put(Context.PROVIDER_URL, "http-remoting://" + host + ":" + port);
-		props.put("remote.connection.default.host", host);
-		props.put("remote.connection.default.port", port);
-	
-		return new InitialContext(props);
+		System.out.println(exampleResult);
 	}
 }
