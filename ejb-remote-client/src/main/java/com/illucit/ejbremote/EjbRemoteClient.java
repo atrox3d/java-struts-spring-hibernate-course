507539265e9cc0043c13ce5854828d48a7f5159e
atrox3d <atrox3d@gmail.com>
2016-02-25 10:10:28 +0100

    cleanup

--- ejb-remote-client/src/main/java/com/illucit/ejbremote/EjbRemoteClient.java
+++ ejb-remote-client/src/main/java/com/illucit/ejbremote/EjbRemoteClient.java
@@ -1,17 +1,10 @@
 package com.illucit.ejbremote;
 
-//import java.util.Hashtable;
 import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-
-//import org.jboss.ejb.client.ContextSelector;
-//import org.jboss.ejb.client.EJBClientConfiguration;
-//import org.jboss.ejb.client.EJBClientContext;
-//import org.jboss.ejb.client.PropertiesBasedEJBClientConfiguration;
-//import org.jboss.ejb.client.remoting.ConfigBasedEJBClientContextSelector;
 
 import com.illucit.ejbremote.server.ExampleService;
 import com.illucit.ejbremote.server.ExampleServiceImpl;
@@ -34,11 +27,6 @@ public class EjbRemoteClient {
 							);
 		
 		System.out.println("getEjbURL() = " + ejbURL);
-		
-		//ejbURL = "ejb:/ejb-remote-server/ExampleServiceImpl!com.illucit.ejbremote.server.ExampleService";
-		
-		//System.out.println("manual URL  = " + ejbURL);
-		
 		System.out.printf("host  : %s\n", host);
 		System.out.printf("port  : %s\n", port);
 		System.out.printf("ejbURL: %s\n", ejbURL);
@@ -48,19 +36,7 @@ public class EjbRemoteClient {
 		remotingContext = EjbRemoteUtil.createRemoteEjbContext(clientProperties);
 		service = EjbRemoteUtil.createEjbProxy(remotingContext, ejbURL, ExampleService.class);
 		
-//	    EJBClientConfiguration ejbClientConfiguration = 
-//	    		new PropertiesBasedEJBClientConfiguration(clientProperties);
-//	    
-//	    ContextSelector<EJBClientContext> contextSelector = 
-//	    		new ConfigBasedEJBClientContextSelector(ejbClientConfiguration);
-//	    
-//	    EJBClientContext.setSelector(contextSelector);
-//	    
-//	    Properties properties = new Properties();
-//	    properties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-//		clientProperties.put("jboss.naming.client.ejb.context", "true");	    
 	    Context context = new InitialContext(clientProperties);
-	    
 	    service = (ExampleService) context.lookup(ejbURL);
 	    
 		String toGreet = "RRrrrobbb!";
