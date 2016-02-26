0ccf7d03d8b68e93d9e7b08925772aafeae431ab
atrox3d <atrox3d@gmail.com>
2016-02-26 09:45:31 +0100

    Add wildfly maven plugin

---
+++ links/wf-bg-shutdown.sh
@@ -0,0 +1,2 @@
+"/c/Program Files/Java/servers/wildfly-8.1.0.Final/bin/jboss-cli.sh" --connect command=:shutdown
+
---
+++ links/wf-bg.sh
@@ -0,0 +1,4 @@
+export JBOSS_HOME="/c/Program Files/Java/servers/wildfly-8.1.0.Final"
+export LAUNCH_JBOSS_IN_BACKGROUND=yes
+sh ./_wildfly-standalone.sh &
+
